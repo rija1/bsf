94806becf5c6a2208ca9caf164eaae47b1471878
rija1 <rija.ratinahirana@gmail.com>
2024-04-14 14:37:20 +0545

    css/js

--- wp-content/themes/twentytwentyfour-child/functions.php
+++ wp-content/themes/twentytwentyfour-child/functions.php
@@ -11,6 +11,6 @@ function my_theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 function my_theme_scripts() {
-    wp_enqueue_script( 'scroll-triggert', get_template_directory_uri() . '/js/ScrollTrigger.min.js');
+    wp_enqueue_script( 'bsf', get_stylesheet_directory_uri() . '/js/bsf.js');
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_scripts' );
