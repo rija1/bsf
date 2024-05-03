3cd61beb3586a06ad8c561ea2d157612c4cbafdc
rija1 <rija.ratinahirana@gmail.com>
2024-05-03 07:23:41 +0545

    remove custom checkout field

--- wp-content/themes/twentytwentyfour-child/functions.php
+++ wp-content/themes/twentytwentyfour-child/functions.php
@@ -18,37 +18,6 @@ add_action( 'wp_enqueue_scripts', 'my_theme_scripts' );
 // Remove Add Coupon Block
 remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 ); 
 
-add_action('woocommerce_after_order_notes', 'custom_checkout_field');
-
-function custom_checkout_field($checkout)
-
-{
-
-echo '<div id="custom_checkout_field"><h3>' . __('Increase your donation by 25% at no extra cost for you') . '</h3>';
-
-woocommerce_form_field('gift_aid_checkbox', array(
-
-'type' => 'checkbox',
-
-
-'class' => array(
-
-'my-field-class form-row-wide'
-
-) ,
-
-'label' => __("I am a taxpayer in the UK and I wish for the Buddhist Support Fund to reclaim tax on any donations I've made in the past four years, as well as on any future donations, until I advise otherwise. I acknowledge that if my Income Tax and/or Capital Gains Tax payments are less than the total Gift Aid claimed on all my donations in a tax year, it is my responsibility to cover the shortfall. Please be aware that Gift Aid applies only to personal donations and cannot be applied to donations collected from others or made on behalf of a company.") ,
-
-'description' => __('') ,
-
-) 			   ,
-
-$checkout->get_value('gift_aid_checkbox'));
-
-echo '</div>';
-
-}
-
 // TODO - REMOVE ON LIVE
 function no_index_cpt()
 {  
