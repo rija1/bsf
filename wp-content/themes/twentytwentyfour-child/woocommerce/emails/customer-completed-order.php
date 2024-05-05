4557de1672b73e25577725fa2d5f0f70987167ed
rija1 <rija.ratinahirana@gmail.com>
2024-05-05 11:23:17 +0545

    test

--- wp-content/themes/twentytwentyfour-child/woocommerce/emails/customer-completed-order.php
+++ wp-content/themes/twentytwentyfour-child/woocommerce/emails/customer-completed-order.php
@@ -58,4 +58,4 @@ if ( $additional_content ) {
 /*
  * @hooked WC_Emails::email_footer() Output the email footer
  */
-do_action( 'woocommerce_email_footer', $email );
+// do_action( 'woocommerce_email_footer', $email );
