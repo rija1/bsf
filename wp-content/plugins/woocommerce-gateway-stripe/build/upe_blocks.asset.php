4256cf7d38fdb09320a068931521ccaa17ded0d5
rija1 <rija.ratinahirana@gmail.com>
2024-07-13 15:16:41 +0545

    woocommerce stripe plugin

---
+++ wp-content/plugins/woocommerce-gateway-stripe/build/upe_blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '56304656a24143849b9bca64f5a26aa7');
